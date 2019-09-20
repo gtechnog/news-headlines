a0b179dceaf4f9b3b53727fec4f74f666bff30ab
ggupta <grvgupta999@gmail.com>
2019-09-20 23:59:33 +0530

    adding dagger as dependency injection

---
+++ app/src/androidTest/java/com/gtechnog/sample/news/ui/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.gtechnog.sample.news.ui;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
---
+++ app/src/main/java/com/gtechnog/sample/news/dagger/ViewModelFactoryModule.java
@@ -0,0 +1,11 @@
+package com.gtechnog.sample.news.dagger;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class SharedNewsViewModelFactoryModule {
+
+    @Provides
+    
+}
